497d2f1d6559cf9f2b4fd9907fd26606c2d80800
Anton Kargapolov <igloooooook@gmail.com>
2023-01-31 23:59:39 +0500

    dz task 10

---
+++ dz_task_010/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8 
+// 918 -> 1
+
+System.Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >99 && number<1000)
+{
+    System.Console.WriteLine((number%100)/10);
+}
+else
+{
+    System.Console.WriteLine("Число не являетя трехзначным");
+}
