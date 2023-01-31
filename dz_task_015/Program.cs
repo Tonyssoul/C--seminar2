28e989e47e4666d4fe6e92b8e3d702ee6087dc3f
Anton Kargapolov <igloooooook@gmail.com>
2023-02-01 00:43:57 +0500

    dz task 15

---
+++ dz_task_015/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да 
+// 7 -> да 
+// 1 -> нет
+
+System.Console.Write("Введите цифру дня недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a>0 && a<8)
+{
+    if(a == 6 || a==7)
+    {
+        System.Console.WriteLine("ДА");
+    }
+    else
+    {
+        System.Console.WriteLine("НЕТ");
+    }
+}
+else
+{
+    System.Console.WriteLine("Такого дня недели нет");
+}
